dc067df908bb6eb5c5946d1446611dd4824bb0ed
silospen <silospen@silospen.com>
2015-12-04 18:27:25 -0800

    Fix hoisting in for loops (yep, all these variables are actually re-used!)

--- app.js
+++ app.js
@@ -111,13 +111,13 @@
 
         busImage.src = 'smallBus.png';
         stopImage.src = 'person.png';
-        for (var x = 0; x < 1600; x += 400) {
-            buses.push(new Bus(x, 150));
+        for (var busInitIndex = 0; busInitIndex < 1600; busInitIndex += 400) {
+            buses.push(new Bus(busInitIndex, 150));
         }
 
         var stopIndex = 0;
-        for (var x = 200; x < 1600; x += 400) {
-            stops.push(new Stop(stopIndex++, x, 200));
+        for (var stopsInitIndex = 200; stopsInitIndex < 1600; stopsInitIndex += 400) {
+            stops.push(new Stop(stopIndex++, stopsInitIndex, 200));
         }
         window.requestAnimationFrame(draw);
     }
@@ -127,22 +127,22 @@
         ctx.clearRect(0, 0, 1600, 800);
 
         // Update buses and draw them
-        for (var i = 0; i < buses.length; ++i) {
-            var bus = buses[i];
+        for (var busDrawIndex = 0; busDrawIndex < buses.length; ++busDrawIndex) {
+            var bus = buses[busDrawIndex];
             bus.draw(ctx);
             bus.tick();
         }
 
         // Update bus stops and draw them
-        for (var i = 0; i < stops.length; ++i) {
-            var stop = stops[i];
+        for (var stopIndex = 0; stopIndex < stops.length; ++stopIndex) {
+            var stop = stops[stopIndex];
             stop.draw(ctx);
             stop.tick();
         }
 
         // Check for buses arriving at stops
-        for (var i = 0; i < buses.length; ++i) {
-            var bus = buses[i];
+        for (var busArrivalIndex = 0; busArrivalIndex < buses.length; ++busArrivalIndex) {
+            var bus = buses[busArrivalIndex];
             for (var j = 0; j < stops.length; ++j) {
                 var stop = stops[j];
                 if (bus.x === stop.x && !stop.busCurrentlyStopped) {
@@ -161,16 +161,16 @@
         yClicked -= canvas.offsetTop;
 
         // Find which stop the click is within, if any
-        for (var i = 0; i < stops.length; ++i) {
-            var stop = stops[i];
+        for (var stopClickIndex = 0; stopClickIndex < stops.length; ++stopClickIndex) {
+            var stop = stops[stopClickIndex];
             if (inside(xClicked, yClicked, stop.boundingBox())) {
                 stop.click();
             }
         }
 
         // Find which bus the click is within, if any
-        for (var i = 0; i < buses.length; ++i) {
-            var bus = buses[i];
+        for (var busClickIndex = 0; busClickIndex < buses.length; ++busClickIndex) {
+            var bus = buses[busClickIndex];
             if (inside(xClicked, yClicked, bus.boundingBox())) {
                 bus.click();
             }
@@ -181,6 +181,7 @@
         if (x > bbox.xmax) return false;
         if (x < bbox.xmin) return false;
         if (y > bbox.ymax) return false;
+        //noinspection RedundantIfStatementJS
         if (y < bbox.ymin) return false;
         return true;
     }
